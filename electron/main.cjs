19bdccccffea4037fe398e71ee3b4b533a63e439
Bram-da-goat <Bram-da-goat@users.noreply.github.com>
2026-08-03 17:05:30 -0700

    Package Legend of Bram as desktop app

---
+++ electron/main.cjs
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 680,
+    title: 'The Legend of Bram',
+    autoHideMenuBar: true,
+    backgroundColor: '#101b22',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+  window.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
